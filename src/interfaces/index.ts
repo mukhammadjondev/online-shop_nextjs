478fccbde88cc25e00b3c30fffd71a8de6cd6c67
mukhammadjondev <muhsdev@gmail.com>
2023-10-26 20:21:32 +0500

    hero and product components

---
+++ src/interfaces/index.ts
@@ -0,0 +1,9 @@
+export interface ProductType {
+  id: number
+  title: string
+  price: number
+  image: string
+  description: string
+  category: string
+  rating: {rate: number, count: number}
+}
